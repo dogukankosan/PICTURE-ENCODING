72cb910cac149b4e673ac406122e8fecf497f543
dogukankosan <77827358+dogukankosan@users.noreply.github.com>
2022-03-25 14:01:58 +0300

    PICTURE ENCODING
    PICTURE ENCODING C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace PictureEncoding64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(1819, 6);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.simpleButton1.Size = new System.Drawing.Size(37, 34);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            this.simpleButton1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.simpleButton1_MouseMove);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(1777, 6);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.simpleButton2.Size = new System.Drawing.Size(36, 35);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            this.simpleButton2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.simpleButton2_MouseMove);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Bilgi";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 79);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(607, 657);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelControl1.Appearance.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseBackColor = true;
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.ImageOptions.Alignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelControl1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("labelControl1.ImageOptions.Image")));
+            this.labelControl1.Location = new System.Drawing.Point(164, 10);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(271, 53);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "    Sifreli Metin:";
+            this.labelControl1.Click += new System.EventHandler(this.labelControl1_Click);
+            this.labelControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.labelControl1_MouseMove);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelControl2.Appearance.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tempus Sans ITC", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseBackColor = true;
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.ImageOptions.Alignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelControl2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("labelControl2.ImageOptions.Image")));
+            this.labelControl2.Location = new System.Drawing.Point(743, 10);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(338, 53);
+            this.labelControl2.TabIndex = 4;
+            this.labelControl2.Text = "   Cözülmüs Metin:";
+            this.labelControl2.Click += new System.EventHandler(this.labelControl2_Click);
+            this.labelControl2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.labelControl2_MouseMove);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox2.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(626, 79);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(607, 657);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
+            this.pictureEdit1.Location = new System.Drawing.Point(1252, 79);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Size = new System.Drawing.Size(476, 451);
+            this.pictureEdit1.TabIndex = 6;
+            this.pictureEdit1.EditValueChanged += new System.EventHandler(this.pictureEdit1_EditValueChanged);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.simpleButton3.Appearance.BackColor = System.Drawing.Color.Blue;
+            this.simpleButton3.Appearance.Font = new System.Drawing.Font("Tempus Sans ITC", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton3.Appearance.Options.UseBackColor = true;
+            this.simpleButton3.Appearance.Options.UseFont = true;
+            this.simpleButton3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.simpleButton3.Location = new System.Drawing.Point(1252, 10);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(237, 53);
+            this.simpleButton3.TabIndex = 7;
+            this.simpleButton3.Text = " Resim Cöz";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            this.simpleButton3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.simpleButton3_MouseMove);
+            // 
+            // Form1
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1863, 748);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.pictureEdit1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.IconOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("Form1.IconOptions.LargeImage")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Base64";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PictureEncoding64
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int mousex, mousey;
+        bool tut=false;
+        string picturelocation;
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            DialogResult cv = new DialogResult();
+            cv = MessageBox.Show("Çıkış Yapmak İstiyor Musunuz ?","Çıkış İşlemi!!",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (cv==DialogResult.Yes)
+            {
+                MessageBox.Show("Çıkış İşlemi Başarılı..", "Çıkış İşlemi!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }      
+        }
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (tut==true)
+            {
+                this.SetDesktopLocation(MousePosition.X - mousex, MousePosition.Y - mousey);
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            tut = true;
+            mousex = e.X;
+            mousey = e.Y;
+        }
+        private void simpleButton1_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(simpleButton1, "Çıkış İşlemi!");
+        }
+        private void simpleButton2_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(simpleButton2, "Aşağı Al!");
+        }
+        private void pictureEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+            picturelocation=pictureEdit1.GetLoadedImageLocation();
+        }
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text=="")
+            {
+                MessageBox.Show("METİN KUTUSUNU BOŞ GEÇMEYİNİZ !!", "BOŞ GEÇMEYİNİZ !!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                richTextBox1.Focus();
+            }
+            else
+            {
+                try
+                {
+                    byte[] veri = Convert.FromBase64String(richTextBox1.Text);
+                    string cöz = ASCIIEncoding.ASCII.GetString(veri);
+                    richTextBox2.Text = cöz;
+                    MessageBox.Show("BAŞARILI ŞEKİLDE ŞİFRE ÇÖZÜLDÜ!!", "BAŞARILI!!", MessageBoxButtons.OK, MessageBoxIcon
+                        .Information);
+                    richTextBox2.Focus();
+                }
+                catch (Exception a)
+                {
+                    MessageBox.Show(a.Message, "HATALI!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    richTextBox1.Text = "";
+                }
+            }      
+        }
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (picturelocation == "")
+                {
+                    MessageBox.Show("RESİM BOŞ GEÇMEYİNİZ RESİM SEÇİNİZ !!", "BOŞ GEÇMEYİNİZ !!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    byte[] imageBytes = System.IO.File.ReadAllBytes(picturelocation);
+                    string base64Formatted = Convert.ToBase64String(imageBytes);
+                    richTextBox2.Text = base64Formatted;
+                    richTextBox2.Focus();
+                    MessageBox.Show("BAŞARILI ŞEKİLDE RESİM ÇÖZÜLDÜ!", "BAŞARILI!", MessageBoxButtons.OK, MessageBoxIcon
+                            .Information);
+               }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message,"RESİM BOŞ GEÇMEYİNİZ RESİM SEÇİNİZ !!",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void labelControl2_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(labelControl2, "Sade Metni Şifreleme Yap!");
+        }
+        private void labelControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(labelControl1, "Şifreli Metni Çöz!");
+        }
+        private void simpleButton3_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.SetToolTip(simpleButton3, "Resim Seçip Şifrelemesini Yapar!");
+        }
+        private void labelControl2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (richTextBox2.Text=="")
+                {
+                    MessageBox.Show("METİN KUTUSUNU BOŞ GEÇMEYİNİZ!!", "HATALI!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    richTextBox2.Focus();
+                }
+                else
+                {
+                    byte[] veri = ASCIIEncoding.ASCII.GetBytes(richTextBox2.Text);
+                    string sifrele = Convert.ToBase64String(veri);
+                    richTextBox1.Text = sifrele;
+                    MessageBox.Show("BAŞARILI ŞEKİLDE ŞİFRELENDİ!", "BAŞARILI!!", MessageBoxButtons.OK, MessageBoxIcon
+                            .Information);
+                    richTextBox1.Focus();
+                }              
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message, "HATALI!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }    
+        }
+       private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            tut = false;         
+        }
+    }
+}
